852d61467ec22dc1b597eba2fff1e9267e4bb742
GeorgeKnap <jirka.knap@gmail.com>
2021-11-14 22:53:25 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from './base/base.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent extends BaseComponent implements OnInit {
+  ngOnInit(): void {
+    this.foo = true;
+  }
 }
