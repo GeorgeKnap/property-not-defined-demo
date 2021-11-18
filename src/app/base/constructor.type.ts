348911b0f6c7ffff4db69fef70952c1ad01a3a23
GeorgeKnap <jirka.knap@gmail.com>
2021-11-18 10:17:59 +0100

    2 files modified

--- src/app/base/constructor.type.ts
+++ src/app/base/constructor.type.ts
@@ -1,1 +1,4 @@
-export type Constructor<T> = new (...args: any[]) => T;
+//export type Constructor<T> = new (...args: any[]) => T;
+
+//fixed by this:
+export type Constructor = new (...args: any[]) => {};
